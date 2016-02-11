ddd8e1f43100e2356495868b9effb57356728747
johnty <johntywang@gmail.com>
2016-02-11 17:12:54 +0000

    minor comments, and check for destination

--- AnalogReadOSC.py
+++ AnalogReadOSC.py
@@ -1,12 +1,14 @@
 import spidev
 import time
-import os
+import os, sys
 import OSC
 
 IP_ADDR='192.168.1.153'
 PORT=7000
 MSG_ADDR='/ICubeX/analog'
 
+
+#initialize the OSC sender. (make sure server is running on other side at the right IP/PORT
 oscSender = OSC.OSCClient()
 oscSender.connect((IP_ADDR,PORT))
 oscMsg = OSC.OSCMessage()
@@ -14,8 +16,8 @@ oscMsg = OSC.OSCMessage()
 
 NUM_CH = 8
 
-spi = spidev.SpiDev()
-spi.open(0,0)
+spi = spidev.SpiDev() #init SPI device
+spi.open(0,0)         #open SPI port 0
 
 def readADC(ch):
    if ( (ch>NUM_CH-1) or (ch<0) ):
@@ -36,7 +38,11 @@ while 1:
          adcValues[ch] = readADC(ch)
          oscMsg.append(val)
       print "ADC Values = ", adcValues
-      oscSender.send(oscMsg) 
+      try:
+         oscSender.send(oscMsg) 
+      except OSC.OSCClientError:
+         print "Error! Unable to connect. Check destination OSC server is running and IP/PORT is correct!\n"
+         sys.exit(0)
    except KeyboardInterrupt:
       break
 
