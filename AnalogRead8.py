69743b9649fac1533dbf48051b965e07a273690c
johnty <johntywang@gmail.com>
2016-05-12 19:11:12 +0000

    minor cleanup

--- AnalogRead8.py
+++ AnalogRead8.py
@@ -2,6 +2,7 @@ import spidev
 import time
 
 NUM_CH = 8
+adcValues = [0 for i in range(NUM_CH)]
 
 spi = spidev.SpiDev()
 spi.open(0,0)
@@ -13,7 +14,6 @@ def readADC(ch):
    val = ((r[1]&3)<<8) + r[2]	
    return val
 
-adcValues = [0 for i in range(8)]
 
 while 1:
    try:
--- AnalogReadOSC.py
+++ AnalogReadOSC.py
@@ -14,6 +14,7 @@ oscMsg = OSC.OSCMessage()
 
 
 NUM_CH = 8
+adcValues = [0 for i in range(NUM_CH)]
 
 spi = spidev.SpiDev() #init SPI device
 spi.open(0,0)         #open SPI port 0
@@ -25,7 +26,6 @@ def readADC(ch):
    val = ((r[1]&3)<<8) + r[2]	
    return val
 
-adcValues = [0 for i in range(8)]
 
 while 1:
    try:
